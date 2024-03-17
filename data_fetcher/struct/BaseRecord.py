985d836025f0386a1eb392f570aea1cbc4a52e7d
motomk2 <moto.rx78@gmail.com>
2024-03-18 00:19:04 +0900

    refactor: 不要クラスを削除

--- data_fetcher/struct/BaseRecord.py
+++
@@ -1,8 +0,0 @@
-class BaseRecord:
-    """基底クラス。共通の表現メソッドを提供する。"""
-
-    def __repr__(self):
-        attributes = ", ".join(
-            [f"{key}={value}" for key, value in self.__dict__.items()]
-        )
-        return f"{self.__class__.__name__}({attributes})"
